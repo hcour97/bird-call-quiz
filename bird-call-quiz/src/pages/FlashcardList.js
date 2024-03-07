134d91bcd5d3ecd4b7d067b890f154ba7f6162fb
Hannah <hcour@gmail.com>
2024-03-07 13:06:19 -0500

    hardcode 5 birdcalls

--- bird-call-quiz/src/pages/FlashcardList.js
+++ bird-call-quiz/src/pages/FlashcardList.js
@@ -1,10 +1,27 @@
 import React from "react";
 
+import bluejay from "../audio/bluejay.mp3";
+import americanrobin from "../audio/americanrobin.mp3";
+import greathornedowl from "../audio/greathornedowl.mp3";
+import northernflicker from "../audio/northernflicker.mp3";
+import northerncardinal from "../audio/northerncardinal.mp3";
+
 function FlashcardList() {
     console.debug("FlashcardList")
+    let birds = [greathornedowl, northernflicker, americanrobin, bluejay, northerncardinal];
     // is flipped useState to show one side (kind of like the applied feature)
+
+    function playBirdCall(bird) {
+        console.debug("playBirdCall", "bird=", bird);
+        new Audio(bird).play();
+    }
     return (
-        <h1>All the Flashcards here! FlashcardList Page!</h1>
+        <div className="FlashcardList">
+            { birds.map(bird => (
+                <button onClick={() => playBirdCall(bird)}>Play Call</button>
+            )) }
+            
+        </div>
     )
 }
 
