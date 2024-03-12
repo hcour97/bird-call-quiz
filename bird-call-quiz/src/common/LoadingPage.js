8df38891a12b8e07db7fd5c42174178b7804d44d
Hannah <hcour@gmail.com>
2024-03-12 12:34:14 -0400

    style loading page

--- bird-call-quiz/src/common/LoadingPage.js
+++ bird-call-quiz/src/common/LoadingPage.js
@@ -6,7 +6,7 @@ import "./loadingpage.css";
 function LoadingPage() {
     return (
         <div className="loading-container">
-            <p>Loading...</p>
+            <p className="loading-text">Loading...</p>
             <img className="loading-image" src={flyingbirds} alt="flying birds" />
         </div>
     )
